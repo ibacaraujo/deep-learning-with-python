2f6dbcee09e7757ddfc535a39d9301de4991c149
Iago Breno <ibacaraujo@gmail.com>
2018-12-11 20:00:26 -0200

    Add naive ReLU function for 1D tensor

--- ch02.py
+++ ch02.py
@@ -1,1 +1,10 @@
 import numpy as np
+
+# Naive ReLU
+def naive_relu(x):
+	res = x.copy()
+	for index, i in enumerate(x):
+		res[index] = max(i, 0) 
+	return res
+	
+print(naive_relu([1, 2, 3, 4, -4, -3, -2, -1]))
