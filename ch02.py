a41f2c335178244912842f2dff6be7ae54f63c63
Iago Breno <ibacaraujo@gmail.com>
2018-12-14 17:16:33 -0200

    Add naive matrix and vector dot function

--- ch02.py
+++ ch02.py
@@ -69,3 +69,16 @@ def naive_vector_dot(x, y):
 	return z
 	
 print(naive_vector_dot(np.array([1, 2]), np.array([1, 2])))
+
+def naive_matrix_vector_dot(x, y):
+	assert len(x.shape) == 2
+	assert len(y.shape) == 1
+	assert x.shape[1] == y.shape[0]
+	
+	z = np.zeros(x.shape[0])
+	for i in range(x.shape[0]):
+		for j in range(x.shape[1]):
+			z[i] += x[i, j] * y[j]
+	return z
+	
+print(naive_matrix_vector_dot(np.array([[1, 2], [3, 4]]), np.array([1, 2])))
