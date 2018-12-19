a418407b4e0c44341c2a8bafc4059cc2f5488168
Iago Breno <ibacaraujo@gmail.com>
2018-12-19 19:03:15 -0200

    Update ch03.py

--- ch03.py
+++ ch03.py
@@ -1,3 +1,7 @@
 from keras import layers
 
 layer = layers.Dense(32, input_shape=(784,)) # A dense layer with 32 output units
+
+model = models.Sequential()
+model.add(layers.Dense(32, input_shape=(784,)))
+model.add(layers.Dense(32))
