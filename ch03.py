aff41c89ff3870ef05d762f59eca1fa1bcb59a06
Iago Breno <ibacaraujo@gmail.com>
2018-12-19 19:15:23 -0200

    Define model using Sequential class

--- ch03.py
+++ ch03.py
@@ -5,3 +5,8 @@ layer = layers.Dense(32, input_shape=(784,)) # A dense layer with 32 output unit
 model = models.Sequential()
 model.add(layers.Dense(32, input_shape=(784,)))
 model.add(layers.Dense(32))
+
+# using the Sequential class
+model = models.Sequential()
+model.add(layers.Dense(32, activation='relu', input_shape=(784,)))
+model.add(layers.Dense(10, activation='softmax'))
