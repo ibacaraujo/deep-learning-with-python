6c298956846476c685e465fd38bbf2a1313fb7a0
Iago Breno <ibacaraujo@gmail.com>
2018-12-19 19:15:45 -0200

    Define model using the functional API

--- ch03.py
+++ ch03.py
@@ -10,3 +10,10 @@ model.add(layers.Dense(32))
 model = models.Sequential()
 model.add(layers.Dense(32, activation='relu', input_shape=(784,)))
 model.add(layers.Dense(10, activation='softmax'))
+
+# same model using the functional API
+input_tensor = layers.Input(shape=(784,))
+x = layers.Dense(32, activation='relu')(input_tensor)
+output_tensor = layers.Dense(10, activation='softmax')(x)
+
+model = models.Model(inputs=input_tensor, outputs=output_tensor)
