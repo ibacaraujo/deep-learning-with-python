d58ad4e8d1ae118ad892c19e391e5115def7ac48
Iago Breno <ibacaraujo@gmail.com>
2018-12-20 20:34:42 -0200

    Compile model

--- ch03.py
+++ ch03.py
@@ -1,4 +1,6 @@
 from keras import layers
+from keras import models
+from keras import optimizers
 
 layer = layers.Dense(32, input_shape=(784,)) # A dense layer with 32 output units
 
@@ -17,3 +19,5 @@ x = layers.Dense(32, activation='relu')(input_tensor)
 output_tensor = layers.Dense(10, activation='softmax')(x)
 
 model = models.Model(inputs=input_tensor, outputs=output_tensor)
+
+model.compile(optimizer=optimizers.RMSprop(lr=0.001), loss='mse', metrics=['accuracy'])
